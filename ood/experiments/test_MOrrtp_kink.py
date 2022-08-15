f0171eb6a12b045e65faa9c71ed16314e8f9ae68
Alonso Marco-Valle <marcovalle.a@gmail.com>
2022-08-15 14:15:31 -0700

    Solved; Gaussian and Student-t models working

--- ood/experiments/test_MOrrtp_kink.py
+++ ood/experiments/test_MOrrtp_kink.py
@@ -100,7 +100,7 @@ def train_test_kink(cfg: dict, block_plot: bool, which_kernel: str) -> None:
 	# spectral_density.update_Wpoints_regular(omega_min,omega_max,Ndiv,normalize_density_numerically=False)
 
 	L = 750.0
-	Ndiv = 501
+	Ndiv = 1201
 	cfg.gpmodel.hyperpars.weights_features.Nfeat = Ndiv**dim_x
 	spectral_density.update_Wpoints_discrete(L,Ndiv,normalize_density_numerically=False,reshape_for_plotting=False)
 
@@ -154,11 +154,11 @@ def train_test_kink(cfg: dict, block_plot: bool, which_kernel: str) -> None:
 	hdl_splots[0].plot(xpred,MO_mean_pred,linestyle="-",color="b",lw=3)
 	# pdb.set_trace()
 	hdl_splots[0].fill_between(xpred[:,0],MO_mean_pred[:,0] - 2.*MO_std_pred[:,0],MO_mean_pred[:,0] + 2.*MO_std_pred[:,0],color="cornflowerblue",alpha=0.5)
+	hdl_splots[0].plot(xplot_true_fun,yplot_true_fun_wrong,marker="None",linestyle="-",color="slategrey",lw=2)
 	hdl_splots[0].plot(xplot_true_fun,yplot_true_fun,marker="None",linestyle="-",color="k",lw=2)
-	hdl_splots[0].plot(xplot_true_fun,yplot_true_fun_wrong,marker="None",linestyle="-",color="grey",lw=2)
 	for ii in range(len(sample_paths_predictive)):
 		hdl_splots[0].plot(xpred,sample_paths_predictive[ii],marker="None",linestyle="--",color="r",lw=0.5)
-	hdl_splots[0].plot(Xtrain[:,0],Ytrain[:,0],marker=".",linestyle="--",color="gray",lw=0.5,markersize=5)
+	hdl_splots[0].plot(Xtrain[:,0],Ytrain[:,0],marker=".",linestyle="--",color="grey",lw=0.5,markersize=5)
 
 	for ii in range(xsamples_X.shape[1]):
 		hdl_splots[0].plot(xsamples_X[0:-1,ii],xsamples_X[1::,ii],marker=".",linestyle="--",lw=0.5,markersize=5)
@@ -167,8 +167,8 @@ def train_test_kink(cfg: dict, block_plot: bool, which_kernel: str) -> None:
 
 	hdl_splots[1].plot(xpred,mean_prior,linestyle="-",color="b",lw=3)
 	hdl_splots[1].fill_between(xpred[:,0],mean_prior[:,0] - 2.*std_prior[:,0],mean_prior[:,0] + 2.*std_prior[:,0],color="cornflowerblue",alpha=0.5)
+	hdl_splots[1].plot(xplot_true_fun,yplot_true_fun_wrong,marker="None",linestyle="-",color="slategrey",lw=2)
 	hdl_splots[1].plot(xplot_true_fun,yplot_true_fun,marker="None",linestyle="-",color="k",lw=2)
-	hdl_splots[1].plot(xplot_true_fun,yplot_true_fun_wrong,marker="None",linestyle="-",color="grey",lw=2)
 	for ii in range(len(sample_paths_prior)):
 		hdl_splots[1].plot(xpred,sample_paths_prior[ii],marker="None",linestyle="--",color="k",lw=0.5)
 	hdl_splots[1].set_xlabel(r"$x_t$",fontsize=fontsize_labels)

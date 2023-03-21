6366ccf8ede81aef674a30bb61eebe06b5f61151
Alonso Marco-Valle <marcovalle.a@gmail.com>
2023-03-21 00:27:54 -0700

    ready to compute predictions in hybridrobotics

--- ood/experiments/test_MOrrtp_quadruped_prediction_module_with_gpflow.py
+++ ood/experiments/test_MOrrtp_quadruped_prediction_module_with_gpflow.py
@@ -212,6 +212,7 @@ def main_train_model(cfg: dict):
 	tf.saved_model.save(model_gpflow, path2save_model_full)
 	logger.info("Done!")
 
+
 @hydra.main(config_path="./config",config_name="config")
 def main_test_model(cfg: dict):
 
@@ -221,9 +222,10 @@ def main_test_model(cfg: dict):
 	Xtrain, Ytrain, state_and_control_full_list, state_next_full_list, dim_x, dim_u, dim_X, using_hybridrobotics, path2project = load_data(cfg)
 
 	# Load model:
-	path2save_model = path2project+"/data_quadruped_experiments_03_13_2023_gpflow/from_hybridrobotics"
+	path2save_model = path2project+"/data_quadruped_experiments_03_13_2023_gpflow"
+	# path2save_model = path2project+"/data_quadruped_experiments_03_13_2023_gpflow/from_hybridrobotics"
 	# model_name = "model_12" # No coregionalization, adding linear kernel
-	model_name = "model_13_coregionalization_True" # not adding linear kernel; optimizaiton finished prematurely
+	model_name = "model_80_coregionalization_True" # not adding linear kernel; optimizaiton finished prematurely
 	path2save_model_full = "{0:s}/{1:s}".format(path2save_model,model_name)
 	logger.info("Loading model from {0:s} ...".format(path2save_model))
 	loaded_model = tf.saved_model.load(path2save_model_full)
@@ -234,45 +236,111 @@ def main_test_model(cfg: dict):
 	Zinduced = loaded_model.get_induced_pointsZ_list()
 	print(Zinduced)
 
-	# Trajectory selector:
-	Ntrajs_for_sel = Xtrain.shape[0]//Nsteps
-	Xtrain_for_sel = tf.reshape(Xtrain,(Ntrajs_for_sel,Nsteps,dim_X))
-	Ytrain_for_sel = tf.reshape(Ytrain,(Ntrajs_for_sel,Nsteps,dim_x))
-	ind_traj_selected = np.random.randint(low=0,high=Ntrajs_for_sel)
-	zu_vec = Xtrain_for_sel[ind_traj_selected,...]
-	zu_next_vec = Ytrain_for_sel[ind_traj_selected,...]
-	z_vec = Xtrain_for_sel[ind_traj_selected,:,0:dim_x].numpy()
-	u_vec = Xtrain_for_sel[ind_traj_selected,:,dim_x::].numpy()
+	# Select test trajectory:
+	ind_which_traj = 1
+	zu_vec = state_and_control_full_list[ind_which_traj]
+	z_vec = zu_vec[:,0:dim_x]
+	u_vec = zu_vec[:,dim_x::]
+	if using_deltas:
+		z_next_vec = state_next_full_list[ind_which_traj] - state_and_control_full_list[ind_which_traj][:,0:dim_x]
+	else:
+		z_next_vec = state_next_full_list[ind_which_traj]
 
 
+	# Plotting:
+	plotting_selected_trajs = False
+	if plotting_selected_trajs and not using_hybridrobotics:
 
 
-	# zu_vec_np = tf.cast(zu_vec,dtype=tf.float64)
-	# MO_mean_pred, var = loaded_model.compiled_predict_f(zu_vec_np)
+		MO_mean_pred, _ = loaded_model.compiled_predict_f(tf.cast(zu_vec,dtype=tf.float64))
 
-	# hdl_fig_pred, hdl_splots_pred = plt.subplots(1,1,figsize=(12,8),sharex=True)
-	# hdl_fig_pred.suptitle("Predictions ...", fontsize=16)
-	# hdl_splots_pred.plot(zu_vec[:,0],zu_vec[:,1],linestyle="-",color="grey",lw=2.0,label=r"Real traj - Input",alpha=0.3)
-	# hdl_splots_pred.plot(zu_next_vec[:,0],zu_next_vec[:,1],linestyle="-",color="navy",lw=2.0,label=r"Real traj - Next state",alpha=0.3)
-	# hdl_splots_pred.plot(MO_mean_pred[:,0],MO_mean_pred[:,1],linestyle="-",color="navy",lw=2.0,label=r"Predicted traj - Next dynamics",alpha=0.7)
+		MO_mean_pred = tf.cast(MO_mean_pred,dtype=tf.float32)
 
-	# plt.show(block=True)
-	# plt.pause(1.)
-
+		if using_deltas:
+			z_next_vec_plotting = z_next_vec + zu_vec[:,0:dim_x]
+			MO_mean_pred_plotting = MO_mean_pred + zu_vec[:,0:dim_x]
+		else:
+			z_next_vec_plotting = z_next_vec
+			MO_mean_pred_plotting = MO_mean_pred
 
 
+		hdl_fig_pred, hdl_splots_pred = plt.subplots(4,figsize=(12,8),sharex=True)
+		hdl_fig_pred.suptitle("Predictions ...", fontsize=16)
+		for ii in range(len(state_and_control_full_list)):
+			zu_el = state_and_control_full_list[ii]
+			zu_el_next = state_next_full_list[ii]
+			hdl_splots_pred[0].cla()
+			hdl_splots_pred[0].plot(zu_el[:,0],zu_el[:,1],linestyle="-",color="grey",lw=3.0,label=r"Real traj - Input",alpha=0.3)
+			hdl_splots_pred[0].plot(zu_el_next[:,0],zu_el_next[:,1],linestyle="-",color="navy",lw=1.0,label=r"Real traj - Input",alpha=0.5)
 
-	# Generate control sequence (u_vec) using the right nominal model, but then apply it to the changed dynamics.
-	z_vec_changed_dyn = np.zeros((Nsteps,dim_x))
-	z_vec_changed_dyn[0,:] = z_vec[0,:]
-	for tt in range(Nsteps-1):
+			hdl_splots_pred[1].cla()
+			hdl_splots_pred[1].plot(zu_el[:,2])
 
-		use_nominal_model = alter_dynamics_flag_time_based(tt,Nsteps)
-		# use_nominal_model = alter_dynamics_flag_state_based(state_curr=z_vec_changed_dyn[tt:tt+1,:])
-		# use_nominal_model = True
+			hdl_splots_pred[2].cla()
+			hdl_splots_pred[2].plot(zu_el[:,3])
 
-		# When using disturbance:
-		z_vec_changed_dyn[tt+1:tt+2,:] = dyn_sys_true(state_vec=z_vec_changed_dyn[tt:tt+1,:],control_vec=u_vec[tt:tt+1,:],use_nominal_model=use_nominal_model,control_vec_prev=None)
+			hdl_splots_pred[3].cla()
+			hdl_splots_pred[3].plot(zu_el[:,4])
+
+			plt.show(block=False)
+
+			# pdb.set_trace()
+			logger.info("ii: {0:d}".format(ii))
+			# plt.pause(0.1)
+			# if ii == 15:
+			# 	plt.show(block=True)
+
+		hdl_fig_pred, hdl_splots_pred = plt.subplots(1,1,figsize=(12,8),sharex=True)
+		hdl_fig_pred.suptitle("Predictions ...", fontsize=16)
+		hdl_splots_pred.plot(z_vec[:,0],z_vec[:,1],linestyle="--",color="grey",lw=1.0,label=r"Real traj - Input",alpha=0.3)
+		hdl_splots_pred.plot(z_next_vec_plotting[:,0],z_next_vec_plotting[:,1],linestyle="-",color="navy",lw=2.0,label=r"Real traj - Next state",alpha=0.3)
+		hdl_splots_pred.plot(MO_mean_pred_plotting[:,0],MO_mean_pred_plotting[:,1],linestyle="-",color="navy",lw=2.0,label=r"Predicted traj - Next dynamics",alpha=0.7)
+
+
+		# plt.show(block=False)
+		plt.show(block=True)
+		plt.pause(4.)
+
+
+	z_vec_tf = tf.convert_to_tensor(value=z_vec,dtype=tf.float32)
+	u_vec_tf = tf.convert_to_tensor(value=u_vec,dtype=tf.float32)
+
+	z_vec_real = z_vec_tf
+	z_vec_changed_dyn_tf = None
+
+	if using_hybridrobotics:
+		# Nhorizon_rec = 40
+		Nhorizon_rec = 25
+		# Nsteps_tot = z_vec_real.shape[0]-Nhorizon_rec
+		Nsteps_tot = z_vec_real.shape[0]
+		# Nsteps_tot = z_vec_real.shape[0]
+		Nepochs = 200
+		Nrollouts = 15
+		Nchunks = 4
+	else:
+
+		# # Nsteps_tot = z_vec_real.shape[0]
+		# Nsteps_tot = 50
+		# Nchunks = 4
+
+		# Nhorizon_rec = 10 # Will be overwritten if Nchunks is passed to get_elbo_loss_for_predictions_in_full_trajectory_with_certain_horizon() and it's not None
+		# Nrollouts = 5
+
+		# # Nsteps_tot = 50
+		# # Nhorizon_rec = 10
+		# # Nrollouts = 5
+
+		# Nepochs = 50
+
+
+		Nhorizon_rec = 25
+		Nsteps_tot = z_vec_real.shape[0]
+		# Nsteps_tot = z_vec_real.shape[0] // 8
+		Nepochs = 200
+		Nrollouts = 40
+		Nchunks = 4
+
+	assert Nsteps_tot > Nhorizon_rec
 
 
 	# Prepare the training and its loss; the latter compares the true trajectory with the predicted one, in chunks.
@@ -285,28 +353,28 @@ def main_test_model(cfg: dict):
 	scale_loss_entropy = 0.1
 	scale_prior_regularizer = 0.1
 	z_vec_tf = tf.convert_to_tensor(value=z_vec,dtype=tf.float32)
-	z_vec_changed_dyn_tf = tf.convert_to_tensor(value=z_vec_changed_dyn,dtype=tf.float32)
+	# z_vec_changed_dyn_tf = tf.convert_to_tensor(value=z_vec_changed_dyn,dtype=tf.float32)
+	z_vec_changed_dyn_tf = None 
 	u_vec_tf = tf.convert_to_tensor(value=u_vec,dtype=tf.float32)
 
 	# compute_predictions_over_trajectory_when_nominal_control_sequence_applied_to = "nominal_model"
-	compute_predictions_over_trajectory_when_nominal_control_sequence_applied_to = "altered_model"
-	assert compute_predictions_over_trajectory_when_nominal_control_sequence_applied_to in ["nominal_model","altered_model"]
-	if compute_predictions_over_trajectory_when_nominal_control_sequence_applied_to == "nominal_model":
-		z_vec_real = z_vec_tf # [Nsteps,dim_out]
-	if compute_predictions_over_trajectory_when_nominal_control_sequence_applied_to == "altered_model":
-		z_vec_real = z_vec_changed_dyn_tf # [Nsteps,dim_out]
+	# # compute_predictions_over_trajectory_when_nominal_control_sequence_applied_to = "altered_model"
+	# assert compute_predictions_over_trajectory_when_nominal_control_sequence_applied_to in ["nominal_model","altered_model"]
+	# if compute_predictions_over_trajectory_when_nominal_control_sequence_applied_to == "nominal_model":
+	# 	z_vec_real = z_vec_tf # [Nsteps,dim_out]
+	# if compute_predictions_over_trajectory_when_nominal_control_sequence_applied_to == "altered_model":
+	# 	z_vec_real = z_vec_changed_dyn_tf # [Nsteps,dim_out]
 
 
 	# Receding horizon predictions:
 	plotting_receding_horizon_predictions = True
 	savedata = True
-	# recompute = True
-	recompute = False
+	recompute = True
+	# recompute = False
 	path2save_receding_horizon = path2project+"/data_quadruped_experiments_03_13_2023_gpflow"
-	file_name = "trajs_ind_traj_{0:d}.pickle".format(ind_traj_selected)
+	file_name = "trajs_ind_traj_{0:d}.pickle".format(my_seed)
 	if plotting_receding_horizon_predictions and recompute:
-		Nhorizon_rec = 10
-		Nsteps_tot = z_vec_real.shape[0]-Nhorizon_rec
+		Nsteps_tot = min(z_vec_real.shape[0]-Nhorizon_rec,Nsteps_tot)
 		loss_val_per_step = np.zeros(Nsteps_tot)
 		x_traj_pred_all_vec = np.zeros((Nsteps_tot,Nrollouts,Nhorizon_rec,dim_x))
 		for tt in range(Nsteps_tot):
@@ -375,7 +443,7 @@ def main_test_model(cfg: dict):
 		# hdl_fig_pred_sampling_rec.suptitle("Simulated trajectory predictions ...", fontsize=fontsize_labels)
 		# hdl_splots_sampling_rec[0].plot(z_vec_real[0:tt+1,0],z_vec_real[0:tt+1,1],linestyle="-",color="navy",lw=2.0,label="Real traj - nominal dynamics",alpha=0.3)
 		hdl_splots_sampling_rec[0].plot(z_vec_tf[:,0],z_vec_tf[:,1],linestyle="-",color="navy",lw=2.0,label="With nominal dynamics",alpha=0.7)
-		hdl_splots_sampling_rec[0].plot(z_vec_changed_dyn_tf[:,0],z_vec_changed_dyn_tf[:,1],linestyle="-",color="navy",lw=2.0,label="With changed dynamics",alpha=0.15)
+		if z_vec_changed_dyn_tf is not None: hdl_splots_sampling_rec[0].plot(z_vec_changed_dyn_tf[:,0],z_vec_changed_dyn_tf[:,1],linestyle="-",color="navy",lw=2.0,label="With changed dynamics",alpha=0.15)
 		hdl_plt_dubins_real, = hdl_splots_sampling_rec[0].plot(z_vec_real[tt,0],z_vec_real[tt,1],marker="*",markersize=14,color="darkgreen",label="Dubins car")
 		# hdl_splots_sampling_rec[0].set_xlim([-6.0,5.0])
 		# hdl_splots_sampling_rec[0].set_ylim([-3.5,1.5])
@@ -431,9 +499,9 @@ def main_test_model(cfg: dict):
 
 if __name__ == "__main__":
 
-	main_train_model()
+	# main_train_model()
 
-	# main_test_model()
+	main_test_model()
 
 
 

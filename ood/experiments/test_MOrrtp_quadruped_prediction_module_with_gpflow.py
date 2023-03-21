3adc79590c08105b128d6ad0ccc46c698c52ef65
Alonso Marco-Valle <marcovalle.a@gmail.com>
2023-03-21 00:35:09 -0700

    wip

--- ood/experiments/test_MOrrtp_quadruped_prediction_module_with_gpflow.py
+++ ood/experiments/test_MOrrtp_quadruped_prediction_module_with_gpflow.py
@@ -312,8 +312,8 @@ def main_test_model(cfg: dict):
 		# Nhorizon_rec = 40
 		Nhorizon_rec = 20
 		# Nsteps_tot = z_vec_real.shape[0]-Nhorizon_rec
-		Nsteps_tot = z_vec_real.shape[0]
 		# Nsteps_tot = z_vec_real.shape[0]
+		Nsteps_tot = z_vec_real.shape[0] // 8
 		Nepochs = 200
 		Nrollouts = 15
 		Nchunks = 4

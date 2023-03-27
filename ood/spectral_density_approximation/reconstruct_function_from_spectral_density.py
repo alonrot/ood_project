15b660f1674a4cc0e6307860d105bd448cc61d65
Alonso Marco-Valle <marcovalle.a@gmail.com>
2023-03-26 19:23:47 -0700

    nomegas 700

--- ood/spectral_density_approximation/reconstruct_function_from_spectral_density.py
+++ ood/spectral_density_approximation/reconstruct_function_from_spectral_density.py
@@ -131,7 +131,7 @@ class ReconstructFunctionFromSpectralDensity(tf.keras.layers.Layer):
 
 		return loss_val
 
-	def train(self,Nepochs,learning_rate,stop_loss_val,lengthscale_loss,print_every=100):
+	def train(self,Nepochs,learning_rate,stop_loss_val,lengthscale_loss,print_every=400):
 
 		# learning_rate_sche = tf.keras.optimizers.schedules.LearningRateSchedule(initial_learning_rate=learning_rate,
 		# 																		decay_steps=10000,
@@ -140,7 +140,7 @@ class ReconstructFunctionFromSpectralDensity(tf.keras.layers.Layer):
 
 		# https://www.tensorflow.org/api_docs/python/tf/keras/optimizers/schedules/CosineDecayRestarts
 		learning_rate_sche = tf.keras.optimizers.schedules.CosineDecayRestarts(	initial_learning_rate=learning_rate,
-																				first_decay_steps=1000,
+																				first_decay_steps=2000,
 																				t_mul=1.0,
 																				m_mul=1.0,
 																				alpha=1e-1)

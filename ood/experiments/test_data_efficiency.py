ce08ed10383225777bfb5848908b4072fa868d8b
Alonso Marco-Valle <marcovalle.a@gmail.com>
2023-03-29 13:58:58 -0700

    minor

--- ood/experiments/test_data_efficiency.py
+++ ood/experiments/test_data_efficiency.py
@@ -341,8 +341,8 @@ def train_gpssm(cfg,ratio):
 	# Xtrain, Ytrain, Xtest, Ytest, dim_in, dim_out, Nsteps, path2data = load_data(path2project,ratio) # Dubins car
 
 	# Based on: https://gpflow.github.io/GPflow/develop/notebooks/advanced/multioutput.html#
-	# MAXITER = reduce_in_tests(1000)
-	MAXITER = 10
+	MAXITER = reduce_in_tests(1000)
+	# MAXITER = 10
 
 	N = Xtrain.shape[0]  # number of points
 	D = Xtrain.shape[1]  # number of input dimensions
